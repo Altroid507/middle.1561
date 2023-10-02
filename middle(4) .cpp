beeaa629a1b8cc0157d5a854d32f556905d1f9e7
Altroid507 <125668171+Altroid507@users.noreply.github.com>
2023-10-02 09:28:50 +0300

    Update middle(4) .cpp

--- middle(4) .cpp
+++ middle(4) .cpp
@@ -3,16 +3,6 @@
 
 using namespace std;
 
-long long step(int num, int step) {
-	int res;
-	res = num;
-	if (step == 0) return 1;
-	for (int i = 2; i <= step; i++) {
-		res = res * num;
-	}
-	return res;
-}
-
 int itc_rev_bin_num(long long number) {
 	int res, cntr;
 	res = 0;
