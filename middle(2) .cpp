3ac54aaadd2fd2d3816f60d0b19c24184f9231db
Altroid507 <125668171+Altroid507@users.noreply.github.com>
2023-10-02 10:39:30 +0300

    Add files via upload

--- middle(2) .cpp
+++ middle(2) .cpp
@@ -1,10 +1,11 @@
-#include "middle.h"
+#include "middle (1).h"
 #include <iostream>
 
 using namespace std;
 
 int itc_min_num(long long number) {
 	int minim;
+	if (number < 0) number = number * -1;
 	minim = number;
 	while (number > 0) {
 		if ((number % 10) < minim) minim = number % 10;
@@ -14,19 +15,27 @@ int itc_min_num(long long number) {
 }
 
 int itc_rev_num(long long number) {
-	int rev;
+	int rev, c;
 	rev = 0;
+	c = 0;
+	if (number < 0) {
+       number = number * -1;
+       c = 1;
+	}
 	while (number > 0) {
 		rev += number % 10;
 		number = number / 10;
 		if (number > 0) rev = rev * 10;
 	}
+	if (c == 1) rev = rev * -1;
 	return rev;
 }
 
 int itc_null_count(long long number) {
 	int countr;
 	countr = 0;
+	if (number == 0) return 1;
+	if (number < 0) number = number * -1;
 	while (number > 0) {
 		if (number % 10 == 0) countr += 1;
 		number = number / 10;
